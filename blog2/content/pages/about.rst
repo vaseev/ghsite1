d6298778b99dc5a68ee48c508ca68db3ba27d712
vaseev <vaseev@gmail.com>
2015-12-28 17:30:04 +0000

    blog2

---
+++ blog2/content/pages/about.rst
@@ -0,0 +1,8 @@
+About me
+########
+:date: 2012-03-30 23:47
+:author: Vasee\.Vinayagamoorthy
+
+This is an about page!
+
+
---
+++ blog2/pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Vasee'
+SITENAME = u'Vasee blog2'
+SITEURL = ''
+
+PATH = 'content'
+PATH_METADATA=''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('twitter', 'http://twitter.com/vaseehh'),
+          ('github', 'http://github.com/vaseev'),
+          ('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
